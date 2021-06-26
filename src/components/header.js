10fcf22094614eef1f4e956bdad150c943bd27d9
Usman Haider <usmanhaider.falconit@gmail.com>
2021-06-26 18:28:18 +0500

    Header Component

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,23 @@
 import * as React from "react"
-import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import Logo from "../images/Logo.png"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+import "./header.css"
+
+const Header = () => (
+  <header>
+    <div className="headerGroup">
+      <Link to="/">
+        <img src={Logo} alt="Logo" />
+      </Link>
+      <Link to="/courses">Courses</Link>
+      <Link to="/downloads">downloads</Link>
+      <Link to="/workshops">Workshops</Link>
+      <Link to="/buy">
+        <button>Buy</button>
+      </Link>
     </div>
   </header>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
