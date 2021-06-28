8f6db8c10e2e10d2031679d9bb3751a4a94921aa
Usman Haider <usmanhaider.falconit@gmail.com>
2021-06-28 09:31:33 +0500

    Asset import Object

---
+++ src/assets/assetImport.js
@@ -0,0 +1,39 @@
+import logo from "./images/Logo.png"
+// Icons
+import figma from "./images/icons/figma-logo.png"
+import sketch from "./images/icons/logo-sketch.png"
+import framer from "./images/icons/logo-framerx.png"
+import react from "./images/icons/logo-react.png"
+import swift from "./images/icons/logo-swift.png"
+import invision from "./images/icons/logo-invision.png"
+import ReactLogo from "./images/icons/Logo.svg"
+import CircularReact from "./images/icons/Logo_React.png"
+// Images
+import wave from "./images/Wave003.png"
+import wave2 from "./images/Wave004.png"
+import wave3 from "./images/Wave005.png"
+import wave4 from "./images/Wave006.png"
+import wave5 from "./images/Wave007.png"
+import wave6 from "./images/Dark001.png"
+import image from "../assets/images/DarkMode0002.png"
+
+const assets = {
+  Logo: logo,
+  Figma: figma,
+  Sketch: sketch,
+  Framer: framer,
+  react: react,
+  Swift: swift,
+  Invision: invision,
+  Wave1: wave,
+  Wave2: wave2,
+  Wave3: wave3,
+  Wave4: wave4,
+  Wave5: wave5,
+  Wave6: wave6,
+  ReactLogo: ReactLogo,
+  Image: image,
+  Circular_React: CircularReact,
+}
+
+export default assets
