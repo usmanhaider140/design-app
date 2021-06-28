f0f551e2df8913aff7d078e51d4a677290f7032b
Usman Haider <usmanhaider.falconit@gmail.com>
2021-06-28 09:33:51 +0500

    Footer Component

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react"
+import styled from "styled-components"
+
+const styledContainer = styled.div`
+  background-color: red;
+  
+`
+
+const Footer = () => {
+  return <styledContainer>H</styledContainer>
+}
+
+export default Footer
